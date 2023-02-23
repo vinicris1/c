4ebb71b00a7cb08391b6f54d598e8f149f819528
vinicris1 <viniciuscris277@gmail.com>
2023-02-23 18:02:43 -0300

    2 e 3

---
+++ 3 - dados.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	
+	printf("aaaaaaaaaaaaaaaaaaaaaa 6 aaaaaa.\n");
+	
+	printf("Valor inteiro: %d.\n", 10);
+	printf("Valor real/float: %f. \n", 3.14159265);
+	printf("Valor real/float com apenas duas casas: %.2f.\n", 3.14159265);
+	printf("Dado de texto: %c. \n", 'a');
+	printf("Dado de texto: %s. \n", "testando");
+	
+}
