b5b50308b7c52b80a6b863da6255586491edcaa4
vinicris1 <viniciuscris277@gmail.com>
2023-03-02 11:24:56 -0300

    Operators

---
+++ operat.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main(){
+    int reminder = 5 / 2;
+    int min = 10;
+    int neg = 5;
+
+    printf("Result = %d\n", reminder);
+    printf("Result = %d\n", -neg - min);
+}
