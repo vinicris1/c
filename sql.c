22dc387aed2ae52221e50c7f18d615e39df9a69f
vinicris1 <viniciuscris277@gmail.com>
2023-03-29 18:26:54 -0300

    sql try in windows, try in linux next time

---
+++ sql.c
@@ -0,0 +1,20 @@
+/*Compile with: gcc sql.c -lmysqlclient -o sql*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <mysql/mysql.h>
+
+void main(){
+    MYSQL conexao;
+    int res;
+    MYSQL_RES *resp;
+    MYSQL_ROW linhas;
+    MYSQL_FIELD *campos;
+    int conta;
+
+    mysql_init(&conexao);
+    if(mysql_real_connect(&conexao, "localhost", "root", "teste", 3306, NULL, 0)){
+        printf("\n CONNECT SUCEFULLY \n");
+    }else{
+    	printf("Connection failure");
+	}
+}
